4d5993bf203c8e02db4065546a28791177755942
DjPatbern <victorbernardpatrick@gmail.com>
2026-07-29 19:06:08 +0100

    first push

--- app/(pages)/portfolio/page.tsx
+++ app/(pages)/portfolio/page.tsx
@@ -1,27 +1,2 @@
-import React from "react";
-import Layout from "@/app/components/Layout";
-import Profile from "./fragments/Profile";
-import Contact from "./fragments/Contact";
-import Skills from "./fragments/Skills";
-import { Metadata } from "next";
-export const metadata: Metadata = {
-  title: 'Portfolio',
-  description: 'This is the portfolio page of Victor Patrick',
-  alternates: {
-    canonical: '/portfolio',
-  },
-};
-
-const Page = () => {
-  return (
-    <Layout>
-      <div className="my-10">
-        <Profile />
-        <Contact />
-        <Skills />
-      </div>
-    </Layout>
-  );
-};
-
-export default Page;
+import { redirect } from "next/navigation";
+export default function Page() { redirect("/#about"); }
