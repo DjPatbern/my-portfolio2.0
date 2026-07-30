5fcf76f93972b66156d19319ac230d860fb606aa
DjPatbern <victorbernardpatrick@gmail.com>
2026-07-30 12:41:06 +0100

    project image sharpening

--- app/page.tsx
+++ app/page.tsx
@@ -177,7 +177,15 @@ export default function Page() {
           <div className="shipGrid">
             {shipped.map((item) => (
               <Link href={item.href} target="_blank" className="shipCard" key={item.name}>
-                <div className="shipImage"><Image src={item.image} alt="" fill sizes="25vw" /></div>
+                <div className="shipImage">
+                  <Image
+                    src={item.image}
+                    alt=""
+                    fill
+                    quality={95}
+                    sizes="(max-width: 460px) calc(100vw - 28px), (max-width: 800px) calc(50vw - 20px), (max-width: 1288px) calc(20vw - 20px), 240px"
+                  />
+                </div>
                 <div><span>{item.label}</span><h3>{item.name}</h3></div><b>↗</b>
               </Link>
             ))}
